afe8cf85d1df672704a8258f8f608174b15eb861
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-10-05 16:38:57 -0700

    🎮 Controller method for GET program-id

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('program-id')
+  getProgramId(){
+    return {result: this.appService.getProgramId()};
+  }
 }
