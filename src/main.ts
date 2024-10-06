9855679d2462b643d93aab9705183d83956d937a
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-10-05 23:27:22 -0700

    😎 Adding Swagger setup

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Solana API')
+    .setDescription('API for interacting with the Solana blockchain')
+    .setVersion('1.0')
+    .addTag('solana')
+    .build();
+
   await app.listen(3000);
 }
 bootstrap();
