5abbb15199051057d40917e416cf5096ecd60ae3
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-10-05 22:51:25 -0700

    🔒 Added private connection

--- src/solana/solana.service.ts
+++ src/solana/solana.service.ts
@@ -2,4 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL, Keypair } from '@solana/web3.js';
 
 @Injectable()
-export class SolanaService {}
+export class SolanaService {
+    private connection: Connection;
+
+}
