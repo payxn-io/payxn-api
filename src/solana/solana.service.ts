eda163937b3f7abe1900baf72f0934f9e872e740
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-10-05 22:49:56 -0700

    📦 Import Connection, PublicKey

--- src/solana/solana.service.ts
+++ src/solana/solana.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL, Keypair } from '@solana/web3.js';
 
 @Injectable()
 export class SolanaService {}
