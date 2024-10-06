ebcaf01109dd784f08b63101d25ee11a019c0c4c
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-10-05 22:52:21 -0700

    🚧 Created construction to establish devnet connection

--- src/solana/solana.service.ts
+++ src/solana/solana.service.ts
@@ -5,4 +5,8 @@ import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL, Keypair } from 
 export class SolanaService {
     private connection: Connection;
 
+    constructor() {
+        // Establish a connection to the Solana devnet
+        this.connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+      }
 }
